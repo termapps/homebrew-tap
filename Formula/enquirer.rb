15aec7ad182ef7ac383a6ceb01da920cac2e9aff
Travis CI User <travis@example.org>
2020-04-26 17:29:37 +0000

    enquirer: update 0.3.1

--- Formula/enquirer.rb
+++ Formula/enquirer.rb
@@ -1,14 +1,14 @@
 class Enquirer < Formula
-  version "0.3.0"
+  version "0.3.1"
   desc "Command Line Utility for Stylish Interactive Prompts"
   homepage "https://github.com/termapps/enquirer"
 
   if OS.mac?
     url "https://github.com/termapps/enquirer/releases/download/v#{version}/enquirer-v#{version}-x86_64-apple-darwin.zip"
-    sha256 "feb0881ec0f23b6fa1fdc8f771b7cfcb7ab16d655f73d8bf216281942075520f" # mac
+    sha256 "c9dfd509c512dd117704859e56171efc4d70ea9ae498460a332bbd6375b11df2" # mac
   elsif OS.linux?
     url "https://github.com/termapps/enquirer/releases/download/v#{version}/enquirer-v#{version}-x86_64-unknown-linux-gnu.zip"
-    sha256 "3a7291613394576a5eb5ef1bfbf78ce82e46552a76096af05131d6435755b4b6" # linux
+    sha256 "a056726680c17fe28745b168b28b4538950dc80897ec81a3570c332a0dd2dd0e" # linux
   end
 
   def install
