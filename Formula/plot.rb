2dab1161cf40d87613b2776f2eb83fc018bdeb4c
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2020-11-04 10:01:21 +0000

    plot: update 0.0.1

--- Formula/plot.rb
+++ Formula/plot.rb
@@ -1,14 +1,14 @@
 class Plot < Formula
-  version "0.0.0"
+  version "0.0.1"
   desc "Command Line Utility for Stylish Interactive Charts"
   homepage "https://github.com/termapps/plot"
 
   if OS.mac?
     url "https://github.com/termapps/plot/releases/download/v#{version}/plot-v#{version}-x86_64-apple-darwin.zip"
-    sha256 "7f260a6ca70fe27f8d20a56cc31b0462b32a63c5acbf65059ed07b2b31f622c3" # mac
+    sha256 "5ed6475a2905bed8a880e6c94c095cfa71160ef748f8e02f9f1a832057e17e0f" # mac
   elsif OS.linux?
     url "https://github.com/termapps/plot/releases/download/v#{version}/plot-v#{version}-x86_64-unknown-linux-gnu.zip"
-    sha256 "c90bffe2b0feb56402009f3c87d05c6e0d9cb0c5f8e9052d4661627dac00bbf5" # linux
+    sha256 "b906bd9708f9e24c538bfe64c125d5dfa27a397b4e5f096c5956569b2c63ab7b" # linux
   end
 
   def install
