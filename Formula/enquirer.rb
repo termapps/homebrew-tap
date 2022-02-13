42e78ffc85a38887f2690422e4ad3c670b4279fe
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2022-02-13 13:08:25 +0000

    enquirer: update 0.6.0

--- Formula/enquirer.rb
+++ Formula/enquirer.rb
@@ -1,14 +1,14 @@
 class Enquirer < Formula
-  version "0.5.0"
+  version "0.6.0"
   desc "Command line utility for stylish interactive prompts"
   homepage "https://github.com/termapps/enquirer"
 
   if OS.mac?
     url "https://github.com/termapps/enquirer/releases/download/v#{version}/enquirer-v#{version}-x86_64-apple-darwin.zip"
-    sha256 "e27b132aae63a1971f9c3b3e183783fcfa805a1530be8b08da2e856466ef19c9" # mac
+    sha256 "13ac98c14b7bf1e804bad957827bbeb782d30d17b0ba49bed64eb7dc04f532d7" # mac
   elsif OS.linux?
     url "https://github.com/termapps/enquirer/releases/download/v#{version}/enquirer-v#{version}-x86_64-unknown-linux-gnu.zip"
-    sha256 "76aa80343f3b653d7ebbdbcc75db8910ad42e46755a585f6eebf539f7d6fb70d" # linux
+    sha256 "8027a5885d888f08a607b7ec578eba5125a7f4dcdb8c9ad93c303c04d4c4501f" # linux
   end
 
   def install
