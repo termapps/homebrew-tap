b113006f13eea36bd5a70f1b39c24156144f4a21
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2024-09-17 13:41:10 +0100

    publisher: 0.1.9

--- Formula/publisher.rb
+++ Formula/publisher.rb
@@ -1,5 +1,5 @@
 class Publisher < Formula
-  version "0.1.8"
+  version "0.1.9"
   desc "Tool to publish & distribute CLI tools"
   homepage "https://github.com/termapps/publisher"
   license "MIT"
@@ -7,14 +7,14 @@ class Publisher < Formula
   if OS.mac?
     if Hardware::CPU.arm?
       url "https://github.com/termapps/publisher/releases/download/v#{version}/publisher-v#{version}-aarch64-apple-darwin.zip"
-      sha256 "ffbdad1d6823a891112665c5d7cbe02520c1e700077d659aa97ac02f03aaa7fd"
+      sha256 "3eac47853faee43c4156a03a3673b41f724e8a6a9feefa5b87b1e473fcba0aa6"
     else
       url "https://github.com/termapps/publisher/releases/download/v#{version}/publisher-v#{version}-x86_64-apple-darwin.zip"
-      sha256 "333f33504876ec4beff6bd6fb71b0a64bec5a9671aa362fb59e2deeaf232e1d8"
+      sha256 "4657b2abd9f33f447b40e5b11c266197ec68b2fbc75a192ea2893010af7f3c23"
     end
   elsif OS.linux?
      url "https://github.com/termapps/publisher/releases/download/v#{version}/publisher-v#{version}-x86_64-unknown-linux-gnu.zip"
-     sha256 "d212fb2d604ba500dab47121cf8fc2f7c2c4bd69b7300994e994358443f28611"
+     sha256 "f9cb6b0295dcb7f8f93ac6de2bb6ff0b20d550b746c5c58f0f61420272100e8d"
   end
 
   def install
